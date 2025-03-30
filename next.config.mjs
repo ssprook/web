23c5c01dfe09b9f8b215d94f635a5139cde9c072
ssprook <zeroqautre@gmail.com>
2025-03-30 10:55:46 +0000

    Ajout du script build et correction du dossier de déploiement

--- next.config.mjs
+++ next.config.mjs
@@ -1,49 +1,7 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',
-//   distDir: 'out',
-//   images: {
-//     unoptimized: true,
-//     domains: [
-//       "source.unsplash.com",
-//       "images.unsplash.com",
-//       "ext.same-assets.com",
-//       "ugc.same-assets.com",
-//     ],
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "source.unsplash.com",
-//         pathname: "/**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "images.unsplash.com",
-//         pathname: "/**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "ext.same-assets.com",
-//         pathname: "/**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "ugc.same-assets.com",
-//         pathname: "/**",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
   distDir: 'out',
-  eslint: {
-    // Ignore les erreurs ESLint pendant la build de production
-    ignoreDuringBuilds: true,
-  },
   images: {
     unoptimized: true,
     domains: [
@@ -78,3 +36,45 @@ const nextConfig = {
 };
 
 export default nextConfig;
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   distDir: 'out',
+//   eslint: {
+//     // Ignore les erreurs ESLint pendant la build de production
+//     ignoreDuringBuilds: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//     domains: [
+//       "source.unsplash.com",
+//       "images.unsplash.com",
+//       "ext.same-assets.com",
+//       "ugc.same-assets.com",
+//     ],
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "source.unsplash.com",
+//         pathname: "/**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "images.unsplash.com",
+//         pathname: "/**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "ext.same-assets.com",
+//         pathname: "/**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "ugc.same-assets.com",
+//         pathname: "/**",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
